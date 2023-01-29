9ebbc88057a5e28c0c44a6e51d941f7544f6d3c6
ruby --version <paulinn.rs@gmail.com>
2023-01-29 19:33:57 -0300

    tipo str

---
+++ aula1.py
@@ -0,0 +1,13 @@
+"""docString
+permite escrever 
+em varias linhas 
+"""
+
+'''docString aspas simples
+Usar para escrever suas notas
+'''
+
+#permite escrever comentario em uma unica linha
+
+print(250+87) #imprime algo na tela
+
---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinãmica / Forte
+str -> string -> texto
+Strngs são textos que estão dentro de aspas
+"""
+
+#Aspas simples
+
+print('ARTHUR')
+print('ARTHUR "RIBEIRO"') # = ESCAPE
+
+#Aspas duplas
+print("PAULINHO")
+print("PAULO 'ROBERTO'") # = ESCAPE
+
+#Escape
+print("ANGELA \"RIBEIRO\"")
+
+#r
+print(r"ANGELA \"RIBEIRO\"")
