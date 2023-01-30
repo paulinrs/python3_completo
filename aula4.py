246c9e9fb5d58cf6064b4f719edd58b0433fcc02
ruby --version <paulinn.rs@gmail.com>
2023-01-30 18:21:27 -0300

    aula4

---
+++ aula4.py
@@ -0,0 +1,24 @@
+#Tipos int  e float
+#int -> Número inteiro
+#O tipo int representa qualquer número
+#positivo ou negativo. int sem sinal é considerado positivo
+
+print(11)#int
+print(-22)#int
+print(0)#int
+
+#float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante.
+# float sem sinal é considerado positivo.
+
+print(2.2, 1.0)#float
+print(-9.7, 19.9)#float
+print(0.0, -1.5)#float
+
+
+# A função type mostra o tipo que o Python inferiu ao valor
+
+print(type('Arthur'))#str
+print(type(0))#int
+print(type(1.5), type(1.5), type(0.0))#float
