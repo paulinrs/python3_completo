31fc69e41614d2e8c9d481d3bf36779d44f35e3c
ruby --version <paulinn.rs@gmail.com>
2023-02-16 18:07:09 -0300

    23.introduçso às variáveis

--- aula2.py
+++ aula2.py
@@ -1,4 +1,4 @@
 print(81, 19, 22, sep='-', end= '->LIBERDADORES\n')
-print(80, 82, 83, 87, 92, 2019, 2020, sep="->", end= '->BRASILEIRO\n')
+print(80, 82, 83, 87, 92, 2019, 2020, sep=".", end= '->BRASILEIRO\n')
 print(90, 2006, 2013, 2022, sep="_", end= '->COPA_DO_BRASIL\n')
-print('TITULOS_DO_FLAMENGO')
+print('TITULOS DO FLAMENGO')
