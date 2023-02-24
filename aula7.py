b163d1c1be3ce78520f66f3429176be07a90be2c
ruby --version <paulinn.rs@gmail.com>
2023-02-24 13:46:17 -0300

    Introdução às variáveis em Python

---
+++ aula7.py
@@ -0,0 +1,20 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar 
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para 
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão 
+
+nome_completo = 'Paulo Roberto dos Santos' 
+soma_dois_mais_dois = 2 + 2
+int_um = int(1)
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Paulo'
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade', idade)
+print('É maior de idade?', maior_de_idade)
+
+
