ae89a56f7a10ed5e99715b7524dcc862ce15e6fb
ruby --version <paulinn.rs@gmail.com>
2023-02-26 17:45:10 -0300

    Usando a função input para coletar dados do usuário

--- aula15.py
+++ aula15.py
@@ -1,10 +1,9 @@
-# nome = input('Qual o seu nome? ')
-# print(f'O seu nome é {nome}')
+nome = input('Qual o seu nome? ')
 
-numero_1 = input('Digite um número: ')
-numero_2 = input('Digite outro número: ')
+numero_1 = input('Digite sua idade: ')
+numero_2 = input('Digite os anos que falta para o termino da sua faculdade: ')
 
 int_numero_1 = int(numero_1)
 int_numero_2 = int(numero_2)
 
-print(f'A soma dos números é: {int_numero_1 + int_numero_2}')
+print(f'Meu nome é {nome}, tenho {numero_1} anos e vou ter {int_numero_1 + int_numero_2} anos quando terminar minha faculdade.')
