82c2e70647210bb208bcd8f0ce5b25dde231f049
ruby --version <paulinn.rs@gmail.com>
2023-03-01 19:14:07 -0300

    Operador Lógico and

---
+++ aula19.py
@@ -0,0 +1,34 @@
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+
+#true
+print(maior)
+print(maior_ou_igual)
+print(menor)
+print(menor_ou_igual)
+print(igual)
+print(diferente)
+
+"""
+python -i NOME DO ARQUIVO (por exemplo python -i aula19.py) entra no modo 
+shell interativo do python
+não sendo necessário utilizar a function print para imprimir 
+só utilizar o nome da variável direto no terminal do vs code
+
+utilizar quit() para sair do modo interativo
+
+"""
